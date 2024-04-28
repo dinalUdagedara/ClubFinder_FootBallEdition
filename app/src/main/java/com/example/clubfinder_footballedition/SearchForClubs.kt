a00bca5c85e9f638f842c7cc43a0785cd1ce237a
Thiyunu <dinal.bandara@gmail.com>
2024-04-28 18:59:40 +0530

    completed The UI for searchClub

--- app/src/main/java/com/example/clubfinder_footballedition/SearchForClubs.kt
+++ app/src/main/java/com/example/clubfinder_footballedition/SearchForClubs.kt
@@ -1,21 +1,17 @@
 package com.example.clubfinder_footballedition
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.clubfinder_footballedition.ui.theme.ClubFinder_FootBallEditionTheme
-import kotlinx.coroutines.launch
 
 class SearchForClubs : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +23,7 @@ class SearchForClubs : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GUI_Club_Serach()
+                    GUI_Club_Search()
                 }
             }
         }
@@ -35,20 +31,23 @@ class SearchForClubs : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
-}
+fun GUI_Club_Search(){
 
-@Composable
-fun GUI_Club_Serach(){
+    var searchTerm by remember { mutableStateOf("") }
+
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row {
-            Column() {
-                TextField(value = "", onValueChange = {  })
+            Column(
+                
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
 
+                TextField(value = searchTerm, onValueChange = { searchTerm = it })
 
                 Button(onClick = {
 
@@ -63,7 +62,7 @@ fun GUI_Club_Serach(){
         }
         Text(
             modifier = Modifier.verticalScroll(rememberScrollState()),
-            text = ""
+            text = searchTerm
         )
 
 
