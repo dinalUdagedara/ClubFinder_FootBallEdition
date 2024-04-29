1e74c57e29e753adcb28e8f3e384f048777246ef
Thiyunu <dinal.bandara@gmail.com>
2024-04-30 01:04:46 +0530

    Fetching is done For All Teams

--- app/src/main/java/com/example/clubfinder_footballedition/SearchForClubWebService.kt
+++ app/src/main/java/com/example/clubfinder_footballedition/SearchForClubWebService.kt
@@ -1,6 +1,7 @@
 package com.example.clubfinder_footballedition
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -21,6 +22,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLEncoder
 
 class SearchForClubWebService : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -84,30 +86,29 @@ fun SearchForClubWebServiceGUI(){
         }
 
         Button(onClick = {
+
+            //To Display fetched TeamInfo
+
             scope.launch {
 
-                val allTeams = fetchAllClubs(keyword)
-                teamsInfo = allTeams.joinToString("\n\n") { team ->
-                    buildString {
-                        append("Team ID: ${team.idTeam}\n")
-                        append("Team Name: ${team.teamName}\n")
-                        append("strTeamShort: ${team.strTeamShort}\n")
-                        append("strAlternate: ${team.strAlternate}\n")
-                        append("intFormedYear: ${team.intFormedYear}\n")
-                        append("strLeague: ${team.strLeague}\n")
-                        append("idLeague: ${team.idLeague}\n")
-                        append("strStadium: ${team.strStadium}\n")
-                        append("strKeywords: ${team.strKeywords}\n")
-                        append("strStadiumThumb: ${team.strStadiumThumb}\n")
-                        append("strStadiumLocation: ${team.strStadiumLocation}\n")
-                        append("intStadiumCapacity: ${team.intStadiumCapacity}\n")
-                        append("strWebsite: ${team.strWebsite}\n")
-                        append("strTeamJersey: ${team.strTeamJersey}\n")
-                        append("strTeamLogo: ${team.strTeamLogo}\n")
+                leagueList.forEach{league ->
+
+                    val allTeams = fetchAllClubs(league.strLeague)
+                    val leagueTeamsInfo = allTeams.joinToString("\n\n") { team ->
+                        buildString {
+                            append("Team ID: ${team.idTeam}\n")
+                            append("Team Name: ${team.teamName}\n")
+                        }
+
                     }
+                    teamsInfo += leagueTeamsInfo + "\n\n" // Add teams info for the current league to the accumulated teamsInfo
+
                 }
+                Log.d("teamNamesinList","$teamsInfo")
+
 
             }
+
 
         }) {
             Text("Fetch Teams")
@@ -115,7 +116,7 @@ fun SearchForClubWebServiceGUI(){
 
 
 
-//        Text(text = teamsList2)
+
         Text(
             modifier = Modifier.verticalScroll(rememberScrollState()),
             text = teamsInfo
@@ -175,12 +176,13 @@ suspend fun fetchAllLeagues(keyword : String): List<Leagues> {
 }
 
 suspend fun fetchAllClubs(keyword : String): List<ClubEntity> {
+    Log.d("Passed Param","League Name: $keyword")
     val teamsList = mutableListOf<ClubEntity>()
 
-//    val encodedKeyword2 = URLEncoder.encode(keyword, "UTF-8")
-//    val url_string = "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=$encodedKeyword2"
+    val encodedKeyword2 = URLEncoder.encode(keyword, "UTF-8")
+    val url_string = "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=$encodedKeyword2"
 
-    val url_string =  "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League"
+//    val url_string =  "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League"
 //    val url_string =  "https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=$keyword"
 
 
@@ -222,10 +224,16 @@ suspend fun fetchAllClubs(keyword : String): List<ClubEntity> {
 
 
             )
+            Log.d("teamName","${team.teamName}")
             teamsList.add(team)
+
+
+
         }
+
     }
 
     return teamsList
+    Log.d("fetchClub","Done")
 }
 
