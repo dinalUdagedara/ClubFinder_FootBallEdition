28c12428c55992174ea53cc12fce31393ba06282
Thiyunu <dinal.bandara@gmail.com>
2024-05-01 00:39:20 +0530

    Found a Way to fetch Jersey Logo but the Iteration has Issues

--- app/src/main/java/com/example/clubfinder_footballedition/SearchForClubWebService.kt
+++ app/src/main/java/com/example/clubfinder_footballedition/SearchForClubWebService.kt
@@ -54,6 +54,9 @@ fun SearchForClubWebServiceGUI(){
     // Creates a CoroutineScope bound to the GUI composable lifecycle
     val scope = rememberCoroutineScope()
 
+    var allTeams = mutableMapOf<String,String>()
+    var jerseyList = mutableListOf<Triple<String, String, String>>()
+
 
 
     Column(
@@ -64,17 +67,7 @@ fun SearchForClubWebServiceGUI(){
         Button(onClick = {
             scope.launch {
 
-                val allLeagues = fetchAllLeagues(keyword)
-                leagueInfo = allLeagues.joinToString("\n\n") { league ->
-                    buildString {
-                        append("idLeague: ${league.idLeague}\n")
-                        append("strLeague: ${league.strLeague}\n")
-                        append("strSport: ${league.strSport}\n")
-                        append("strLeagueAlternate: ${league.strLeagueAlternate}\n")
 
-
-                    }
-                }
 //                leagueList.forEach{league ->
 //
 //                    val allTeams = fetchAllClubs(league.strLeague)
@@ -91,20 +84,54 @@ fun SearchForClubWebServiceGUI(){
 
             scope.launch {
 
-                leagueList.forEach{league ->
+                val allLeagues = fetchAllLeagues(keyword)
+                leagueInfo = allLeagues.joinToString("\n\n") { league ->
+                    buildString {
+                        append("idLeague: ${league.idLeague}\n")
+                        append("strLeague: ${league.strLeague}\n")
+                        append("strSport: ${league.strSport}\n")
+                        append("strLeagueAlternate: ${league.strLeagueAlternate}\n")
 
-                    val allTeams = fetchAllClubs(league.strLeague)
-                    val leagueTeamsInfo = allTeams.joinToString("\n\n") { team ->
-                        buildString {
-                            append("Team ID: ${team.idTeam}\n")
-                            append("Team Name: ${team.teamName}\n")
-                        }
 
                     }
-                    teamsInfo += leagueTeamsInfo + "\n\n" // Add teams info for the current league to the accumulated teamsInfo
+                }
+
+
+                val teamsInfoStringBuilder = StringBuilder()
+                leagueList.forEach{league ->
+
+                     allTeams = fetchAllClubs(league.strLeague,searchTerm)
+//                    val leagueTeamsInfo = allTeams.joinToString("\n\n") { team ->
+//                        buildString {
+//                            append("Team ID: ${team.idTeam}\n")
+//                            append("Team Name: ${team.teamName}\n")
+//                        }
+//
+//                    }
+//                    teamsInfo += leagueTeamsInfo + "\n\n" // Add teams info for the current league to the accumulated teamsInfo
+
+                    allTeams.forEach { (key, value) ->
+                        teamsInfoStringBuilder.append("Team ID: $key\n")
+                        teamsInfoStringBuilder.append("Team Name: $value\n\n")
+
+
+                    }
+
+                    val teamsInfoString = teamsInfoStringBuilder.toString()
+                    teamsInfo = teamsInfoString
+
+                    Log.d("allTeams in function","$allTeams")
 
                 }
-                Log.d("teamNamesinList","$teamsInfo")
+
+                allTeams.forEach{(key,value)->
+                    jerseyList = lookupJerseys(key,value)
+                }
+                Log.d("allTeams","$allTeams")
+                Log.d("Jersey List","$jerseyList")
+                Log.d("teamNamesInList","$teamsInfo")
+
+
 
 
             }
@@ -121,10 +148,10 @@ fun SearchForClubWebServiceGUI(){
             modifier = Modifier.verticalScroll(rememberScrollState()),
             text = teamsInfo
         )
-        Text(
-            modifier = Modifier.verticalScroll(rememberScrollState()),
-            text = leagueInfo
-        )
+//        Text(
+//            modifier = Modifier.verticalScroll(rememberScrollState()),
+//            text = leagueInfo
+//        )
 
 
     }
@@ -175,9 +202,10 @@ suspend fun fetchAllLeagues(keyword : String): List<Leagues> {
     return leagueList
 }
 
-suspend fun fetchAllClubs(keyword : String): List<ClubEntity> {
+suspend fun fetchAllClubs(keyword : String,searchTerm:String): MutableMap<String,String> {
     Log.d("Passed Param","League Name: $keyword")
     val teamsList = mutableListOf<ClubEntity>()
+    val teamMap = mutableMapOf<String,String>()
 
     val encodedKeyword2 = URLEncoder.encode(keyword, "UTF-8")
     val url_string = "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=$encodedKeyword2"
@@ -192,6 +220,64 @@ suspend fun fetchAllClubs(keyword : String): List<ClubEntity> {
 
 
     withContext(Dispatchers.IO) {
+        try {
+            val bf = BufferedReader(InputStreamReader(con.inputStream))
+            val stb = StringBuilder()
+            var line: String? = bf.readLine()
+            while (line != null) {
+                stb.append(line + "\n")
+                line = bf.readLine()
+            }
+
+            val json = JSONObject(stb.toString())
+            if (json.has("teams")) {
+                val jsonArray = json.getJSONArray("teams")
+
+                for (i in 0 until jsonArray.length()) {
+                    val leagueObject = jsonArray.getJSONObject(i)
+                    val idTeam = leagueObject.getString("idTeam")
+                    val teamName1 = leagueObject.getString("strTeam")
+
+                    if (teamName1.contains("$searchTerm")) {
+                        teamMap["$idTeam"] = "$teamName1"
+                    }
+                }
+            } else {
+                Log.d("FetchClubsError", "No 'teams' field found in the JSON response")
+            }
+
+
+        } catch (e: Exception) {
+            Log.e("FetchClubsError", "Error fetching clubs: ${e.message}")
+        }
+    }
+
+
+
+    return teamMap
+    Log.d("fetchClub","Done")
+}
+
+
+
+
+suspend fun lookupJerseys(teamID : String, teamName: String) :MutableList<Triple<String, String, String>> {
+
+    val jerseysList = mutableListOf<Triple<String, String, String>>()
+
+    val url_string = "https://www.thesportsdb.com/api/v1/json/3/lookupequipment.php?id=$teamID"
+
+//    val url_string =  "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League"
+//    val url_string =  "https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=$keyword"
+
+
+//    val url_string = "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=Eliteserien"
+    val url = URL(url_string)
+    val con: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+
+
+    withContext(Dispatchers.IO) {
         val bf = BufferedReader(InputStreamReader(con.inputStream))
         val stb = StringBuilder()
         var line: String? = bf.readLine()
@@ -201,39 +287,28 @@ suspend fun fetchAllClubs(keyword : String): List<ClubEntity> {
         }
 
         val json = JSONObject(stb.toString())
-        val jsonArray = json.getJSONArray("teams")
+        val jsonArray = json.getJSONArray("equipment")
 
         for (i in 0 until jsonArray.length()) {
-            val leagueObject = jsonArray.getJSONObject(i)
-            val team = ClubEntity(
-                idTeam = leagueObject.getString("idTeam"),
-                teamName = leagueObject.getString("strTeam"),
-                strTeamShort = leagueObject.getString("strTeamShort"),
-                strAlternate = leagueObject.getString("strAlternate"),
-                intFormedYear = leagueObject.getString("intFormedYear"),
-                strLeague = leagueObject.getString("strLeague"),
-                idLeague = leagueObject.getString("idLeague"),
-                strStadium = leagueObject.getString("strStadium"),
-                strKeywords = leagueObject.getString("strKeywords"),
-                strStadiumThumb = leagueObject.getString("strStadiumThumb"),
-                strStadiumLocation = leagueObject.getString("strStadiumLocation"),
-                intStadiumCapacity = leagueObject.getString("strLeague"),
-                strWebsite = leagueObject.getString("strWebsite"),
-                strTeamJersey = leagueObject.getString("strTeamJersey"),
-                strTeamLogo = leagueObject.getString("strTeamLogo")
+            val jerseyObject = jsonArray.getJSONObject(i)
 
+            val strSeason = jerseyObject.getString("strSeason")
+            val strEquipment = jerseyObject.getString("strEquipment")
 
-            )
-            Log.d("teamName","${team.teamName}")
-            teamsList.add(team)
+            val teamName = teamName
+
+            jerseysList.add(Triple(teamName, strSeason, strEquipment))
+
 
 
 
         }
 
+        Log.d("in JerseyFun","$jerseysList")
     }
 
-    return teamsList
-    Log.d("fetchClub","Done")
+
+    return jerseysList
+
 }
 
